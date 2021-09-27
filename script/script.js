975c1cd839d127c252a92dd059cfd87b80c60197
FranGuerre <franguerrero054@gmail.com>
2021-09-27 15:08:43 -0300

    commit para push

--- script/script.js
+++ script/script.js
@@ -89,9 +89,10 @@ let solapas = document.querySelectorAll(".solapa");
 let alerta = document.getElementById("alerta");
 let nav = document.querySelector(".header-mobile");
 let navMenu = document.querySelector(".nav-menu");
+let navToggle = document.querySelector(".nav-toggle");
 
 window.addEventListener("DOMContentLoaded", function() {
-  navToggle();
+  navFuncionalidad();
   cargarSolapas();
   funcionalidadSolapas();
   funcionalidadFiltros();
@@ -99,10 +100,10 @@ window.addEventListener("DOMContentLoaded", function() {
 
 // header-mobile
 
-function navToggle() {
+function navFuncionalidad() {
   nav.addEventListener("click", (e) => {
-    if(e.target.classList.contains("nav-toggle")) {
-      e.target.classList.toggle("active");
+    if(e.target.classList.contains("nav-toggle") || e.target.classList.contains("icon-bar")) {      
+      navToggle.classList.toggle("active");
       navMenu.classList.toggle("mostrar-nav");
       nav.classList.toggle("background-active");
     }
